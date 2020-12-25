6055270e981968a696de1036ce8fbcf624a08f4f
alian1483 <69890980+alian1483@users.noreply.github.com>
2020-12-25 15:14:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    if (mbit_小車類.Avoid_Sensor(mbit_小車類.enAvoidState.OBSTACLE)) {
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Stop, 0)
+    } else {
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 41)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
 })
@@ -135,4 +142,10 @@ basic.forever(function () {
         # . . . #
         # # # # #
         `)
+})
+basic.forever(function () {
+    mbit_小車類.RGB_Car_Big2(mbit_小車類.enColor.Blue)
+    basic.pause(100)
+    mbit_小車類.RGB_Car_Big2(mbit_小車類.enColor.OFF)
+    basic.pause(1000)
 })
